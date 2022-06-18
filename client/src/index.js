ea1ff04523de516d437c2d9a789eb9b7acb2fa9d
juhyeon-an <ajh2042@naver.com>
2022-06-18 16:41:54 +0900

    Add : socket added

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AuthService from './service/auth';
-import TweetService from './service/tweet';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { AuthErrorEventBus } from './context/AuthContext';
-import HttpClient from './network/http';
-import TokenStorage from './db/token';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthService from "./service/auth";
+import TweetService from "./service/tweet";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { AuthErrorEventBus } from "./context/AuthContext";
+import HttpClient from "./network/http";
+import TokenStorage from "./db/token";
+import Socket from "./network/socket";
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 const tokenStorage = new TokenStorage();
 const authErrorEventBus = new AuthErrorEventBus();
 const httpClient = new HttpClient(baseURL, authErrorEventBus);
 const authService = new AuthService(httpClient, tokenStorage);
-const tweetService = new TweetService(httpClient, tokenStorage);
+const socketClient = new Socket(baseURL, () => tokenStorage.getToken()); // 별도로 tokenStorage를 연결하지 않고 읽기만 하도록 콜백함수 전달
+const tweetService = new TweetService(httpClient, tokenStorage, socketClient);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider
-        authService={authService}
-        authErrorEventBus={authErrorEventBus}
-      >
+      <AuthProvider authService={authService} authErrorEventBus={authErrorEventBus}>
         <App tweetService={tweetService} />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,35 +1,37 @@
 export default class TweetService {
-  constructor(http, tokenStorage) {
+  constructor(http, tokenStorage, socket) {
     this.http = http;
     this.tokenStorage = tokenStorage;
+    this.socket = socket;
   }
 
   async getTweets(username) {
-    const query = username ? `?username=${username}` : '';
+    const query = username ? `?username=${username}` : "";
     return this.http.fetch(`/tweets${query}`, {
-      method: 'GET',
+      method: "GET",
       headers: this.getHeaders(),
     });
   }
 
   async postTweet(text) {
+    console.log("postTweet : ", text);
     return this.http.fetch(`/tweets`, {
-      method: 'POST',
+      method: "POST",
       headers: this.getHeaders(),
-      body: JSON.stringify({ text, username: 'ellie', name: 'Ellie' }),
+      body: JSON.stringify({ text }),
     });
   }
 
   async deleteTweet(tweetId) {
     return this.http.fetch(`/tweets/${tweetId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: this.getHeaders(),
     });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`/tweets/${tweetId}`, {
-      method: 'PUT',
+      method: "PUT",
       headers: this.getHeaders(),
       body: JSON.stringify({ text }),
     });
@@ -40,5 +42,9 @@ export default class TweetService {
     return {
       Authorization: `Bearer ${token}`,
     };
+  }
+
+  onSync(callback) {
+    return this.socket.onSync("tweets", callback);
   }
 }
