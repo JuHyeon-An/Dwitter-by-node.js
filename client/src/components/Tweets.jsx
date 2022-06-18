20f69850369aa871f006df8acbd6c8c2db32ac06
juhyeon-an <ajh2042@naver.com>
2022-06-18 16:43:33 +0900

    Fix : bug fixed

--- client/src/components/Tweets.jsx
+++ client/src/components/Tweets.jsx
@@ -16,9 +16,6 @@ const Tweets = memo(({ tweetService, username, addable }) => {
       .getTweets(username)
       .then((tweets) => setTweets([...tweets]))
       .catch(onError);
-
-    const stopSync = tweetService.onSync((tweet) => onCreated(tweet));
-    return () => stopSync();
   }, [tweetService, username, user]);
 
   const onCreated = (tweet) => {
@@ -50,7 +47,9 @@ const Tweets = memo(({ tweetService, username, addable }) => {
 
   return (
     <>
-      {addable && <NewTweetForm tweetService={tweetService} onError={onError} />}
+      {addable && (
+        <NewTweetForm tweetService={tweetService} onError={onError} onCreated={onCreated} />
+      )}
       {error && <Banner text={error} isAlert={true} transient={true} />}
       {tweets.length === 0 && <p className="tweets-empty">No Tweets Yet</p>}
       <ul className="tweets">
